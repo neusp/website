9aa244fb3c91928e44a73d2d7b01143b326a4037
Rodrigo Alves Lima <rodrigoalveslima@gmail.com>
2012-02-03 17:19:30 -0200

    Displaying background

--- neu/cms/views.py
+++ neu/cms/views.py
@@ -5,12 +5,12 @@ from cms.models import Page, MenuItem, MenuSubitem
 from django.shortcuts import render_to_response, get_object_or_404
 
 def main(request):
-    menu_items = MenuItem.objects.all()
     page = get_object_or_404(Page, url=request.path)
-    menu_item = page.menus.get()
+    templates = page.templates.all()
+    menu_item = get_object_or_404(MenuItem, page=page)
+    menu_items = MenuItem.objects.all()
     return render_to_response('base.html',
                               {'menu_items': menu_items,
                                'page': page,
+                               'templates': templates,
                                'menu_item': menu_item})
-    #return HttpResponse(request.path)
-    #raise Http404()
--- neu/urls.py
+++ neu/urls.py
@@ -1,9 +1,18 @@
 from django.conf.urls.defaults import patterns, include, url
 
 from django.contrib import admin
+import settings
 admin.autodiscover()
 
-urlpatterns = patterns('',
+if settings.DEBUG:
+    urlpatterns = patterns('',
+                            (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+                                {'document_root': settings.MEDIA_ROOT,
+                                'show_indexes':True}),
+    )
+
+
+urlpatterns += patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('neu.cms.urls')),
 )
